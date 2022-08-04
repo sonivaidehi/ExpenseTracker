73c0f973eb337eb45761cb28685c16b006548357
sonivaidehi <sonivn116@gmail.com>
2022-08-04 21:23:33 +0530

    basic of props

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import './ExpenseItem.css';
+const ExpenseItem = (props) => {
+
+  return (
+    <div className='expense-item'>
+      <div>{props.expense.date.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{props.expense.title}</h2>
+        <div className='expense-item__price'>{props.expense.amount}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
